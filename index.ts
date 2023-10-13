85a509888993df040512e40deb5165524c739ee2
Julien Rousseau <julrousseau20@gmail.com>
2023-10-13 16:12:11 -0400

    added zod support

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export * as typebox from "./src/typebox.js"
 export * as entity_pb from "./src/generated/sf/substreams/sink/entity/v1/entity_pb.js";
+export * as typebox from "./src/typebox.js";
+export * as zod from "./src/zod.js";
