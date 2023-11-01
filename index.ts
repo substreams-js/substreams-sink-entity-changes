1f4afb8ba1fe9933bd17de80174b09b037656f80
Denis Carriere <carriere.denis@gmail.com>
2023-11-01 11:44:13 -0400

    remove entity_bp from default export

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export * from "./src/extract-values.js";
-export * as entity_pb from "./src/generated/sf/substreams/sink/entity/v1/entity_pb.js";
 export * as typebox from "./src/typebox.js";
 export * as zod from "./src/zod.js";
